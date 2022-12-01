492f10f70cae9c7ee1ff9db19d134494b72f74b6
Cristian Mello <cristianc.mello@gmail.com>
2022-11-30 23:20:09 -0300

    retomando valores do exemplo

--- src/test/java/com/simplebank/service/SimpleBankTests.java
+++ src/test/java/com/simplebank/service/SimpleBankTests.java
@@ -9,12 +9,12 @@ import static org.assertj.core.api.Assertions.*;
 public class SimpleBankTests {
     @Test
     void dadaContaCorrenteAComSaldo_eOutraContaCorrenteBSaldo_quandoAtransferirValorB_entaoContasSaldosComputados() {
-        var contaCorrenteA = new ContaCorrente(BigDecimal.valueOf(0.3));
-        var contaCorrenteB = new ContaCorrente(BigDecimal.valueOf(0));
+        var contaCorrenteA = new ContaCorrente(BigDecimal.valueOf(1200.0));
+        var contaCorrenteB = new ContaCorrente(BigDecimal.valueOf(300.0));
 
-        contaCorrenteA.transfere(contaCorrenteB, BigDecimal.valueOf(0.1));
+        contaCorrenteA.transfere(contaCorrenteB, BigDecimal.valueOf(380.0));
 
-        assertThat(contaCorrenteA.getSaldo()).isEqualTo(BigDecimal.valueOf(0.2));
-        assertThat(contaCorrenteB.getSaldo()).isEqualTo(BigDecimal.valueOf(0.1));
+        assertThat(contaCorrenteA.getSaldo()).isEqualTo(BigDecimal.valueOf(820.0));
+        assertThat(contaCorrenteB.getSaldo()).isEqualTo(BigDecimal.valueOf(680.0));
     }
 }
