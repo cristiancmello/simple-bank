07d469f47c758025b819bc18da09f555b88ecb12
Cristian Mello <cristianc.mello@gmail.com>
2022-11-30 23:13:09 -0300

    novo exemplo com erro grave : IEEE 754 causa erros de representacao

--- src/test/java/com/simplebank/service/SimpleBankTests.java
+++ src/test/java/com/simplebank/service/SimpleBankTests.java
@@ -2,17 +2,19 @@ package com.simplebank.service;
 
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.*;
 
 public class SimpleBankTests {
     @Test
     void dadaContaCorrenteAComSaldo_eOutraContaCorrenteBSaldo_quandoAtransferirValorB_entaoContasSaldosComputados() {
-        var contaCorrenteA = new ContaCorrente(1200.0);
-        var contaCorrenteB = new ContaCorrente(300.0);
+        var contaCorrenteA = new ContaCorrente(0.3);
+        var contaCorrenteB = new ContaCorrente(0);
 
-        contaCorrenteA.transfere(contaCorrenteB, 380.0);
+        contaCorrenteA.transfere(contaCorrenteB, 0.1);
 
-        assertThat(contaCorrenteA.getSaldo()).isEqualTo(820.0);
-        assertThat(contaCorrenteB.getSaldo()).isEqualTo(680.0);
+        assertThat(contaCorrenteA.getSaldo()).isEqualTo(0.2);
+        assertThat(contaCorrenteB.getSaldo()).isEqualTo(0.1);
     }
 }
