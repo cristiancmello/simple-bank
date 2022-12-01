274883ae5b41f07cc25851403d8b2d98ea7a8077
Cristian Mello <cristianc.mello@gmail.com>
2022-11-30 22:47:21 -0300

    First commit

---
+++ src/main/java/com/simplebank/service/SimpleBankApplication.java
@@ -0,0 +1,13 @@
+package com.simplebank.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/simplebank/service/SimpleBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplebank.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
